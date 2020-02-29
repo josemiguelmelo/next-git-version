58b0634fdd4a4928d4efe6ea19fbc1a1dc845b38
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-02-29 15:33:59 +0000

    Add prefix option

--- generate.sh
+++ generate.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
+PREFIX=$1
 
 initial_version="0.0.1"
-last_version=$(git describe --tags $(git rev-list --tags --max-count=1))
+last_version=$(git describe --tags $(git rev-list --tags --max-count=1) | sed 's/[A-Za-z]*//g')
 major="$(cut -d'.' -f1 <<<$last_version)"
 minor="$(cut -d'.' -f2 <<<$last_version)"
 patch="$(cut -d'.' -f3 <<<$last_version)"
 
 next_version=""
-commit_msg=$(git log -1 --pretty=%B | sed "s/ *$//g")
+commit_msg=$(git log -1 --pretty=%B)
 major_comment=":breaking:"
 minor_comment="feature:"
 
 if [ -z $major ] && [ -z $minor ] && [ -z $patch ]; then
     next_version=$initial_version
 else
-    if [[ $commit_msg == *$major_comment ]]; then
+    if [[ $commit_msg == *$major_comment* ]]; then
         major=$(($major+1))
-    elif [[ $commit_msg == $minor_comment* ]]; then
+    elif [[ $commit_msg == *$minor_comment* ]]; then
         minor=$(($minor+1))
     else
         patch=$(($patch+1))
@@ -24,4 +25,4 @@ else
     next_version="$major.$minor.$patch"
 fi
 
-printf $next_version
+printf "$PREFIX$next_version"
